1b6f0142f47cc753d4502fa9a955ab21792452bc
josef-davi <142513205+josef-davi@users.noreply.github.com>
2023-08-31 11:20:07 -0300

    Create library_problem_4.py

---
+++ .github/library_problem_4.py
@@ -0,0 +1,23 @@
+def main():
+    def resultado():
+      def e_perfeito(n):
+        soma = 0
+        for i in range(1, n):
+            if n % i == 0:
+                soma += i
+        return soma == n
+
+    def testar_numeros_perfeitos():
+        n = int(input("Digite quantos testes realizará: "))
+        for i in range(1, n+1):
+            x = int(input(f"Teste {i}: "))
+        if e_perfeito(x):
+                print(f"{x} é perfeito.")
+          else:
+                print(f"{x} não é perfeito.")
+
+    testar_numeros_perfeitos()
+
+
+if __name__ == '__main__':
+    main()
