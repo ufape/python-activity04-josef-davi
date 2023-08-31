383f0454f03b996183bb292505e83a707ae021a8
josef-davi <142513205+josef-davi@users.noreply.github.com>
2023-08-31 11:20:39 -0300

    Update problem_4.py

--- problem_4.py
+++ problem_4.py
@@ -31,8 +31,29 @@ Teste 3: 28
 
 
 def main():
-    pass  # your code here
+    def main():
+    def resultado():
+      def e_perfeito(n):
+        soma = 0
+        for i in range(1, n):
+            if n % i == 0:
+                soma += i
+        return soma == n
+
+    def testar_numeros_perfeitos():
+        n = int(input("Digite quantos testes realizará: "))
+        for i in range(1, n+1):
+            x = int(input(f"Teste {i}: "))
+        if e_perfeito(x):
+                print(f"{x} é perfeito.")
+          else:
+                print(f"{x} não é perfeito.")
+
+    testar_numeros_perfeitos()
 
 
 if __name__ == '__main__':
     main()
+
+
+
