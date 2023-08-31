66a649e75742a519d4a9e49c5dfaff25d023dd76
josef-davi <142513205+josef-davi@users.noreply.github.com>
2023-08-31 11:14:45 -0300

    library_problem_2.py

---
+++ .github/library_problem_2.py
@@ -0,0 +1,17 @@
+def main():
+  x = int(input("Digite o valor 1: "))
+  y = int(input("Digite o valor 2: "))
+
+  if x > y:
+        x, y = y, x
+    numeros = []
+    for i in range(x+1, y):
+        if i % 5 == 2 or i % 5 == 3:
+            numeros.append(i)
+
+    print("Os números são..: {}".format(" ".join(str(n) for n in numeros)))
+            
+
+
+
+if __name__ == '__main__':
